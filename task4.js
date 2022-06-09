d931d4e080fb54b0102013bb0d0d16169e1acec7
MURUGAN SEKAR <murugansekar2608@gmail.com>
2022-06-09 21:29:38 +0530

    task4

---
+++ task4.js
@@ -0,0 +1,6 @@
+var lists = document.getElementsByClassName('list-group-item');
+lists[2].style.backgroundColor = 'green';
+for(var i=0;i<lists.length;i++)
+{
+    lists[i].style.fontWeight = 'bold';
+}
