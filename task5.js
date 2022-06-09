2788bae8ca696c34dcb44c62c7a8c17415909b6f
MURUGAN SEKAR <murugansekar2608@gmail.com>
2022-06-09 22:28:35 +0530

    task 5

---
+++ task5.js
@@ -0,0 +1,11 @@
+var lis = document.getElementsByTagName('li');
+for(var i=0;i<lis.length;i++)
+{
+    lis[i].style.backgroundColor = 'yellow';
+}
+var lists = document.getElementsByClassName('list-group-item');
+for(var i=0;i<lists.length;i++)
+{
+    lists[i].style.fontWeight = 'bold';
+}
+
